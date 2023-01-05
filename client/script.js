7839a0e8e40619c072a11f75da2b51a278fa266c
pooja mulik <52532142+pmulik34@users.noreply.github.com>
2023-01-05 16:46:18 +0530

    function generates a unique ID for eavery answer by combining a timestamp, a random number, and a hexadecimal string.

--- client/script.js
+++ client/script.js
@@ -40,6 +40,40 @@ function textType(element, text){
   },20);
 }
 
+function textType(element, text){
+  // Set up an interval to run every 20 milliseconds
+  let index = 0;
+  let interval = setInterval(()=>{
+    // If the index is less than the length of the text
+    if(index < text.lenght){
+      // Add the character at the current index of the text to the innerHTML of the element
+      element.innerHT += text.chartAt(index);
+      // Increment the index by 1
+      index++;
+    }else{
+      // If the index is not less than the length of the text, clear the interval
+      clearInterval(interval);
+    }
+  },20);
+}
+
+// This function generates a unique ID by combining a timestamp, a random number, and a hexadecimal string.
+function generateUniqueId() {
+
+  // Get the current time as a timestamp (number of milliseconds since January 1, 1970).
+  const timeStamp = Date.now();
+
+  // Generate a random number between 0 and 1.
+  const randomNumber = Math.random();
+
+  // Convert the random number to a hexadecimal string (base 16).
+  const hexadecimalString = randomNumber.toString(16);
+
+  // Return the combined ID string, using template literals.
+  return`id-${timeStamp}-${hexadecimalString}`;
+}
+
+
 
 
 
